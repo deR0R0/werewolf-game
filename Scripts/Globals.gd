6395961c2b3c2d23cf984502242d9029814ec133
Theophylaktos <91230260+PearlAndChai@users.noreply.github.com>
2025-11-02 09:04:31 -0500

    Added Werewolf Logic
    The logic for giving werewolf responses, a global variable that says who is the werewolf have both been added.
    
    The code for the dice rolls has also been simplified to only require 2 dice to be rolled compared to the previous 6 per character

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -1,16 +1,37 @@
 extends Node
 
+# Werewolf
+
+var werewolf: int = randi_range(1, 4)
+# 1 = Margaret
+# 2 = Alan
+# 3 = Selene
+# 4 = Theodore
+
 # Double Route Variables
 
 var margaret_intimidation: bool = true
 var margaret_persuasion: bool = false
 
+var theodore_deception: bool = true
+
 # Dice Rolling
+	# Margaret Dice Rolls
+var marg_roll_r1: int = randi_range(1, 20)
 
-var marg_pers_r1: int = randi_range(1, 20)
-var marg_dece_r1: int = randi_range(1, 20)
-var marg_inti_r1: int = randi_range(1, 20)
+var marg_roll_r2: int = randi_range(1, 20)
 
-var marg_pers_r2: int = randi_range(1, 20)
-var marg_dece_r2: int = randi_range(1, 20)
-var marg_inti_r2: int = randi_range(1, 20)
+	# Theodore Dice Rolls
+var theo_roll_r1: int = randi_range(1, 20)
+
+var theo_roll_r2: int = randi_range(1, 20)
+
+	# Selene Dice Rolls
+var sele_roll_r1: int = randi_range(1, 20)
+
+var sele_roll_r2: int = randi_range(1, 20)
+
+	# Alan Dice Rolls
+var alan_roll_r1: int = randi_range(1, 20)
+
+var alan_roll_r2: int = randi_range(1, 20)
