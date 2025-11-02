dfd16bcf1bc0ae64ec136d75c1cfa0a573d31f77
Theophylaktos <91230260+PearlAndChai@users.noreply.github.com>
2025-11-02 00:02:54 -0400

    Dialogue System, Theo Sprites
    Using this dialogue manager: https://github.com/nathanhoad/godot_dialogue_manager
    
    The basic logic for passing/failing checks, choosing different routes, and double routes has been implemented.
    
    Theodore's sprites have also been added

---
+++ Scripts/Globals.gd
@@ -0,0 +1,16 @@
+extends Node
+
+# Double Route Variables
+
+var margaret_intimidation: bool = true
+var margaret_persuasion: bool = false
+
+# Dice Rolling
+
+var marg_pers_r1: int = randi_range(1, 20)
+var marg_dece_r1: int = randi_range(1, 20)
+var marg_inti_r1: int = randi_range(1, 20)
+
+var marg_pers_r2: int = randi_range(1, 20)
+var marg_dece_r2: int = randi_range(1, 20)
+var marg_inti_r2: int = randi_range(1, 20)
