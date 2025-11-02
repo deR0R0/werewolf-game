e0d20d2c9849a887e8809b9274980154b3ebc639
Theophylaktos <91230260+PearlAndChai@users.noreply.github.com>
2025-11-02 11:17:16 -0500

    Added Balloon to game
    The balloon is now visible when you click the play button.

--- Scripts/Suspect.gd
+++ Scripts/Suspect.gd
@@ -1,10 +1,11 @@
 extends Sprite2D
 
+@onready var R1_Margaret_Dialogue = load("res://dialogues/R1_Margaret_Dialogue.dialogue")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	DialogueManager.show_dialogue_balloon(R1_Margaret_Dialogue, "start")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
