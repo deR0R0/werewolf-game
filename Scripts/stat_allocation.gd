57080cb43c9c8b41de79c974576c1fa4e9e22100
deR0R0 <rorozee1@gmail.com>
2025-11-08 18:45:52 -0500

    feat(stat): add a beautiful stat selection scene

--- Scripts/stat_allocation.gd
+++ Scripts/stat_allocation.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+@onready var bars = [$PersuasionBar/BarIndicator, $IntimidationBar/BarIndicator, $DeceptionBar/BarIndicator]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,54 +10,55 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
+	
+func _update_bars():
+	bars[0].texture = load("res://Assets/Stat_Allocation/bar-" + str(Global.persuasion_stat) + ".png")
+	bars[1].texture = load("res://Assets/Stat_Allocation/bar-" + str(Global.intimidation_stat) + ".png")
+	bars[2].texture = load("res://Assets/Stat_Allocation/bar-" + str(Global.deception_stat) + ".png")
+	
+	
+func _increase_values(stat, amount):
+	if Global[stat + "_stat"] >= 5:
+		return # maxed out?
+		
+	if Global.allocatable_points <= 0:
+		return # no more allocated points
+		
+	Global[stat + "_stat"] += amount
+	Global.allocatable_points -= amount
+	
+	print("INCREASED ", stat, " STAT. CURRENT: ", str(Global[stat + "_stat"]), " ALLOCATED: ", str(Global.allocatable_points)) # debug
+	
+	_update_bars()
+	
+func _decrease_values(stat, amount):
+	if Global[stat + "_stat"] <= 0:
+		return # min'd out?
+		
+	Global[stat + "_stat"] -= amount
+	Global.allocatable_points += amount
+	
+	print("DECREASED ", stat, " STAT. CURRENT: ", str(Global[stat + "_stat"]), " ALLOCATED: ", str(Global.allocatable_points)) # debug
+	
+	_update_bars()
+	
+func _on_persuasion_increase_pressed():
+	_increase_values("persuasion", 1)
 
-
-func _on_persuasion_up_button_down():
-	if Global.allocatable_points > 0 and Global.persuasion_stat < 5:
-		Global.persuasion_stat = Global.persuasion_stat + 1
-		Global.allocatable_points = Global.allocatable_points - 1
-		print(Global.persuasion_stat)
-		print(Global.allocatable_points)
-
-func _on_deception_up_button_down():
-	if Global.allocatable_points > 0 and Global.deception_stat < 5:
-		Global.deception_stat = Global.deception_stat + 1
-		Global.allocatable_points = Global.allocatable_points - 1
-		print(Global.deception_stat)
-		print(Global.allocatable_points)
-
-
-func _on_interrogation_up_button_down():
-	if Global.allocatable_points > 0 and Global.intimidation_stat < 5:
-		Global.intimidation_stat = Global.intimidation_stat + 1
-		Global.allocatable_points = Global.allocatable_points - 1
-		print(Global.intimidation_stat)
-		print(Global.allocatable_points)
-
-
-
-
-func _on_persuasion_down_button_down():
-	if Global.allocatable_points < 9 and Global.persuasion_stat > 0:
-		Global.persuasion_stat = Global.persuasion_stat - 1
-		Global.allocatable_points = Global.allocatable_points + 1
-		print(Global.persuasion_stat)
-		print(Global.allocatable_points)
-
-func _on_deception_down_button_down():
-	if Global.allocatable_points < 9 and Global.deception_stat > 0:
-		Global.deception_stat = Global.deception_stat - 1
-		Global.allocatable_points = Global.allocatable_points + 1
-		print(Global.deception_stat)
-		print(Global.allocatable_points)
-
-func _on_interrogation_down_button_down():
-	if Global.allocatable_points < 9 and Global.intimidation_stat > 0:
-		Global.intimidation_stat = Global.intimidation_stat - 1
-		Global.allocatable_points = Global.allocatable_points + 1
-		print(Global.intimidation_stat)
-		print(Global.allocatable_points)
-
+func _on_persuasion_decrease_pressed() -> void:
+	_decrease_values("persuasion", 1)
+	
+func _on_intimidation_increase_pressed() -> void:
+	_increase_values("intimidation", 1)
+	
+func _on_intimidation_decrease_pressed() -> void:
+	_decrease_values("intimidation", 1)
+	
+func _on_deception_increase_pressed() -> void:
+	_increase_values("deception", 1)
+	
+func _on_deception_decrease_pressed() -> void:
+	_decrease_values("deception", 1)
 
 func _on_continue_button_down():
 	Global.Stats_selected = true
