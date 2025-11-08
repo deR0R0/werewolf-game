b18444a0fc75dbd4a9afe63654f5e4f01ec521e3
deR0R0 <rorozee1@gmail.com>
2025-11-08 16:53:27 -0500

    feat(intermission): add intermission between stat and lineup + tip

--- Scripts/intermission_script.gd
+++ Scripts/intermission_script.gd
@@ -1,11 +1,14 @@
-extends Sprite2D
+extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	await get_tree().create_timer(4.0).timeout
 	GlobalMusicPlayer.play_music("interrogation")
-	get_tree().change_scene_to_file("res://scenes/Lineup.tscn")
+	if Global.persuasion_stat + Global.intimidation_stat + Global.deception_stat == 0: # condition for if player hasn't chosen stats yet
+		get_tree().change_scene_to_file("res://Scenes/stat_allocation.tscn")
+	else:
+		get_tree().change_scene_to_file("res://scenes/Lineup.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
