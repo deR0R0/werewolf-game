9511a1b60e900a39a19e843276fd5e4b287034e9
deR0R0 <rorozee1@gmail.com>
2025-11-08 19:30:16 -0500

    feat(stat): major update. too much to list

--- Scripts/stat_allocation.gd
+++ Scripts/stat_allocation.gd
@@ -2,26 +2,60 @@ extends Node2D
 
 @onready var bars = [$PersuasionBar/BarIndicator, $IntimidationBar/BarIndicator, $DeceptionBar/BarIndicator]
 
+var shown_warning = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	_update_bars() # just to fill out the remaintingpoints
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
 	
+	
+func _cannot_do_stupid_dumbass(bar):
+	match bar:
+		"persuasion":
+			bar = bars[0]
+		"intimidation":
+			bar = bars[1]
+		"deception":
+			bar = bars[2]
+		_:
+			printerr("I don't even know how this could happen")
+	
+	if bar.has_meta("tween") and bar.get_meta("tween").is_valid():
+		return
+	
+	# for when you can't increase/decrease anymore
+	var bar_pos = bar.position
+	var tween = create_tween()
+	bar.set_meta("tween", tween)
+	
+	tween.tween_property(bar, "position", bar_pos + Vector2(-15, 0), 0.075)
+	tween.tween_property(bar, "position", bar_pos + Vector2(15, 0), 0.15)
+	tween.tween_property(bar, "position", bar_pos, 0.075)
+	
+	bar.modulate = Color(1, 0.3, 0.3)
+	await get_tree().create_timer(0.2).timeout
+	bar.modulate = Color(1, 1, 1)
+
+	
 func _update_bars():
 	bars[0].texture = load("res://Assets/Stat_Allocation/bar-" + str(Global.persuasion_stat) + ".png")
 	bars[1].texture = load("res://Assets/Stat_Allocation/bar-" + str(Global.intimidation_stat) + ".png")
 	bars[2].texture = load("res://Assets/Stat_Allocation/bar-" + str(Global.deception_stat) + ".png")
+	$RemainingPoints.text = "Remaining Points: " + str(Global.allocatable_points)
 	
 	
 func _increase_values(stat, amount):
 	if Global[stat + "_stat"] >= 5:
+		_cannot_do_stupid_dumbass(stat)
 		return # maxed out?
 		
 	if Global.allocatable_points <= 0:
+		_cannot_do_stupid_dumbass(stat)
 		return # no more allocated points
 		
 	Global[stat + "_stat"] += amount
@@ -33,6 +67,7 @@ func _increase_values(stat, amount):
 	
 func _decrease_values(stat, amount):
 	if Global[stat + "_stat"] <= 0:
+		_cannot_do_stupid_dumbass(stat)
 		return # min'd out?
 		
 	Global[stat + "_stat"] -= amount
@@ -61,5 +96,10 @@ func _on_deception_decrease_pressed() -> void:
 	_decrease_values("deception", 1)
 
 func _on_continue_button_down():
+	if !shown_warning and Global.allocatable_points > 0:
+		$WarningLabel.visible = true
+		shown_warning = true
+		return
+	
 	Global.Stats_selected = true
 	get_tree().change_scene_to_file("res://scenes/Lineup.tscn")
