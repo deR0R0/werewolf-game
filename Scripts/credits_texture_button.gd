3874ffae5a27e7f8f61a2a3b0535a8fca4b97a9d
Theophylaktos <91230260+PearlAndChai@users.noreply.github.com>
2025-11-09 20:54:56 -0500

    Pretty-fied Main menu and credits

---
+++ Scripts/credits_texture_button.gd
@@ -0,0 +1,21 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	if texture_normal:
+		# Get the image from the texture normal
+		var image = texture_normal.get_image()
+		# Create the BitMap
+		var bitmap = BitMap.new()
+		# Fill it from the image alpha
+		bitmap.create_from_image_alpha(image)
+		# Assign it to the mask
+		texture_click_mask = bitmap
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+	
+func _on_button_down():
+	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
